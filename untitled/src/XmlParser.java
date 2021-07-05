d74bc8b7e307d0f2104b0a89677749ef81ee6c86
iwantrisenup <yaopeng3034@stu.xjtu.edu.cn>
2021-07-05 14:03:24 +0800

    Add files via upload
    first commit

---
+++ untitled/src/XmlParser.java
@@ -0,0 +1,21 @@
+import com.sun.org.apache.xml.internal.resolver.readers.SAXCatalogReader;
+import sun.plugin.dom.exception.WrongDocumentException;
+
+//import org.dom4j.DocumentException;
+import java.io.IOException;
+import java.util.List;
+
+public class XmlParser
+{
+    private static final String FILTER = "filter";
+    private static final String DESTINATION = "destination";
+
+
+//    public static Element loadFile(String mapFilePath,String logFilePath) throws WrongDocumentException, IOException
+//    {
+//        List<String> list = LogFileParse.loadLogFile((logFilePath));//载入文件路径
+//
+//
+//    }
+
+}
